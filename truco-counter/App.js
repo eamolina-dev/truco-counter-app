4681a688938621747721911b08b45f283ebceb31
EAMolina500 <molinaenzoagustin@gmail.com>
2025-03-27 21:47:32 -0300

    Remove navigation

--- truco-counter/App.js
+++ truco-counter/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <AppNavigator />
+      <GameScreen />
     </SafeAreaProvider>
   );
 };
